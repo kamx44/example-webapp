aa8a9dcac68b5e7b3eff51fee964ab699dce57c2
Kamil <kamx44@gmail.com>
2019-10-23 23:25:31 +0200

    run stack changed

--- run-stack.sh
+++ run-stack.sh
@@ -7,6 +7,6 @@ aws cloudformation $ACTION \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameters \
     ParameterKey=DockerImage,ParameterValue=819816673551.dkr.ecr.us-east-1.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
-    ParameterKey=VPC,ParameterValue=vpc-c43f74be
+    ParameterKey=VPC,ParameterValue=vpc-c43f74be \
     ParameterKey=Cluster,ParameterValue=default \
     ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-east-1:819816673551:listener/app/production-website/a54931960a315fbc/57cadc0f28784b59
